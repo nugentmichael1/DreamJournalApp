9c73a5dc03eec89f760587bae0b9e144ea52aacf
Michael Nugent <nugentmichael@live.com>
2024-08-01 16:12:09 -0700

    manually imported anup's user model, controller, and serviceImpl.

--- projects/Dreamjournel/src/main/java/com/revature/Controllers/UserController.java
+++ projects/Dreamjournel/src/main/java/com/revature/Controllers/UserController.java
@@ -1,4 +1,22 @@
 package com.revature.Controllers;
+import com.revature.Models.Dream;
+import com.revature.Models.User;
+import com.revature.Services.DreamService;
+import com.revature.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@RestController
 public class UserController {
+    UserService US;
+
+    @Autowired
+    public UserController(UserService US) {
+        this.US = US;
+    }
 }
--- projects/Dreamjournel/src/main/java/com/revature/Models/User.java
+++ projects/Dreamjournel/src/main/java/com/revature/Models/User.java
@@ -2,16 +2,51 @@ package com.revature.Models;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Column(name="userid")
+    int u_id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
 
     @OneToMany(mappedBy = "user")
     private List<Dream> dreams;
+
+    public User(int u_id, String username, String password) {
+        this.u_id = u_id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setU_id(int u_id) {
+        this.u_id = u_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
