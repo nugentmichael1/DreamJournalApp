be88fe096fb8de4adbf5a0881e9f94fedc4265a2
Michael Nugent <nugentmichael@live.com>
2024-08-02 13:45:16 -0700

    Corrected refactor issues

--- projects/Dreamjournel/src/main/java/com/revature/models/User.java
+++ projects/Dreamjournel/src/main/java/com/revature/models/User.java
@@ -1,4 +1,5 @@
 package com.revature.models;
+
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -7,40 +8,18 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
+@Setter
+@Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="userid")
+    @Column(name = "userid")
     int u_id;
     @Column(nullable = false)
     private String username;
     @Column(nullable = false)
     private String password;
 
-
-   // @OneToMany(mappedBy = "user")
-   //private List<Dream> dreams;
-
-
-
-
-    public void setU_id(int u_id) {
-        this.u_id = u_id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    // @OneToMany(mappedBy = "user")
+    //private List<Dream> dreams;
 }
--- projects/Dreamjournel/src/main/java/com/revature/services/UserServiceImpl.java
+++ projects/Dreamjournel/src/main/java/com/revature/services/UserServiceImpl.java
@@ -1,7 +1,8 @@
 package com.revature.services;
+
 import com.revature.models.User;
-import com.revature.repositories.UserRepo;
 import com.revature.repositories.DreamRepo;
+import com.revature.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,9 @@ public class UserServiceImpl implements UserService {
     public List<User> getAllUser() {
         return UR.findAll();
     }
+
     @Override
-    public User saveUser(User user){
+    public User saveUser(User user) {
         return UR.save(user);
     }
 }
