5206041f49ecccb1947c6155f869c53934f5ca20
Michael Nugent <nugentmichael@live.com>
2024-08-03 19:41:52 -0700

    feat: authentication JWTs

--- projects/Dreamjournel/src/main/java/com/revature/models/User.java
+++ projects/Dreamjournel/src/main/java/com/revature/models/User.java
@@ -2,23 +2,37 @@ package com.revature.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
-@Setter
-@Getter
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userid")
+    @Column(name = "user_id")
     int u_id;
     @Column(nullable = false)
     private String username;
     @Column(nullable = false)
     private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 
     // @OneToMany(mappedBy = "user")
     //private List<Dream> dreams;
